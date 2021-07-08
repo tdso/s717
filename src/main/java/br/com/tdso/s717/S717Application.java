ba97308c1099b28e089e77d13bdd1fe3575c1cea
tdso <tdso.oliveira@gmail.com>
2021-07-08 19:31:55 -0300

    estrutura inicial projeto

---
+++ src/main/java/br/com/tdso/s717/S717Application.java
@@ -0,0 +1,13 @@
+package br.com.tdso.s717;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S717Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S717Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tdso/s717/S717ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tdso.s717;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S717ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
